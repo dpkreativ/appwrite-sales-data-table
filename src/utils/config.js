9f29af6c798f193b25df68ad34a97c0585831ebd
Divine Orji <dpkreativ@gmail.com>
2022-09-01 12:26:26 +0100

    configure Appwrite keys

---
+++ src/utils/config.js
@@ -0,0 +1,6 @@
+export const Server = {
+  endpoint: process.env.VUE_APP_ENDPOINT,
+  project: process.env.VUE_APP_PROJECT,
+  collectionId: process.env.VUE_APP_COLLECTION_ID,
+  database: process.env.VUE_APP_DATABASE_ID,
+};
